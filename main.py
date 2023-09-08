34934314d931d3db1c3286117581703a1238113b
ali <najdawiali369@gmail.com>
2023-09-08 20:49:22 +0300

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+size = 5  # specify the size of the list
+my_list = []
+
+# Input values into the list
+for i in range(size):
+    value = int(input(f"Enter value for index {i}: "))
+    my_list.append(value)
+
+print(my_list)
